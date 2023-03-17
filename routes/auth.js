2b1d920a49aac57d2afa3066b65e9739dcda33d7
shubhcode12 <shubhcode.dev@gmail.com>
2023-03-18 00:31:06 +0530

    setup auth route

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const { signUp, signIn } = require("../controller/authController");
+const {  
+  isRequestValidated,
+  validateSignUpRequest,
+  validateSignIpRequest,
+} = require("../validators/auth");
+
+
+router.route("/signin").post(validateSignIpRequest, isRequestValidated, signIn);
+
+
+router.route("/signup").post(validateSignUpRequest, isRequestValidated, signUp);
+
+
+
+module.exports = router;
