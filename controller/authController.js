e6b25efeca2cac7df66c0ca958d80862a55bbd76
shubhcode12 <shubhcode.dev@gmail.com>
2023-03-20 01:34:15 +0530

    Add : setup signin & signup auth fun

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,9 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/user.model");
+const Admin = require("../models/admin.model");
 const jwt = require("jsonwebtoken");
 const shortid = require("shortid");
+const { default: mongoose } = require("mongoose");
 
 
 const signUp = async (req, res) => {
@@ -13,29 +15,26 @@ const signUp = async (req, res) => {
     }
 
 
-    const userData = {
-        name,
-        address,
-        company,
-        email,
-        phone,
-        password,
-    };
+    const user = new User({
+        _id: new mongoose.Types.ObjectId,
+        name: req.body.name,
+        address: req.body.address,
+        company: req.body.company,
+        email: req.body.email,
+        phone: req.body.phone,
+        password: req.body.password,
+    });
+    
+    user.save().then((result) => {
+        res
+            .status(200)
+            .json({ newUser: result });
 
-    const user = await User.findOne({ email });
-    if (user) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            message: "User already registered",
-        });
-    } else {
-        User.create(userData).then((data, err) => {
-            if (err) res.status(StatusCodes.BAD_REQUEST).json({ err });
-            else
-                res
-                    .status(StatusCodes.CREATED)
-                    .json({ message: "User created Successfully" });
-        });
-    }
+    }).catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    });
 };
 
 const signIn = async (req, res) => {
@@ -47,28 +46,78 @@ const signIn = async (req, res) => {
 
         }
 
-        const user = await User.findOne({ email: req.body.email });
+        User.findOne({
+            email: req.body.email,
+        }).then((user) => {
 
-        if (user) {
-            if (user.authenticate(req.body.password)) {
+            if (!user) {
+                return res.status(404).send({ message: "User Not found." });
+            }
 
-                const { _id, name, address, company, email, phone } = user;
-                res.status(StatusCodes.OK).json({
-                    user: { _id, name, address, company, email, phone, },
-                });
-            } else {
-                res.status(StatusCodes.UNAUTHORIZED).json({
-                    message: "Something went wrong!",
+            if (req.body.password != user.password) {
+                return res.status(401).send({ message: "Invalid Password!" });
+            }else{
+                res.status(200).json({
+                    _id: user._id,
+                    name: user.name,
+                    address: user.address,
+                    company: user.company,
+                    email: user.email,
+                    phone: user.phone,                
                 });
             }
-        } else {
-            res.status(StatusCodes.BAD_REQUEST).json({
-                message: "User does not exist..!",
-            });
-        }
+
+        }).catch((err) => {
+            if (err) {
+                res.status(500).send({ message: err });
+                return;
+            }
+        });
+
+
     } catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json({ error });
     }
 };
 
-module.exports = { signUp, signIn };
+const adminSignin = async (req, res) => {
+    try {
+        if (!req.body.email || !req.body.password) {
+            res.status(StatusCodes.BAD_REQUEST).json({
+                message: "Please enter email and password",
+            });
+
+        }
+
+        Admin.findOne({
+            email: req.body.email,
+        }).then((admin) => {
+
+            if (!admin) {
+                return res.status(404).send({ message: "Admin Not found." });
+            }
+
+            if (req.body.password != admin.password) {
+                return res.status(401).send({ message: "Invalid Password!" });
+            }else{
+                res.status(200).json({
+                    _id: admin._id,
+                    name: admin.name,
+                    email: admin.email,                              
+                });
+            }
+
+        }).catch((err) => {
+            if (err) {
+                res.status(500).send({ message: err });
+                return;
+            }
+        });
+
+
+    } catch (error) {
+        res.status(StatusCodes.BAD_REQUEST).json({ error });
+    }
+}
+
+module.exports = { signUp, signIn, adminSignin };
