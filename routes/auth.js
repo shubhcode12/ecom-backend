72a3944e0fc3c1e1a5646bd5c4cc9a54cbbc726e
shubhcode12 <shubhcode.dev@gmail.com>
2023-03-20 01:34:54 +0530

    fix : auth route and add admin route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { signUp, signIn } = require("../controller/authController");
+const { signUp, signIn , adminSignin } = require("../controller/authController");
+const User = require("../models/user.model");
 const {  
   isRequestValidated,
   validateSignUpRequest,
@@ -10,9 +11,8 @@ const {
 
 router.route("/signin").post(validateSignIpRequest, isRequestValidated, signIn);
 
-
 router.route("/signup").post(validateSignUpRequest, isRequestValidated, signUp);
 
-
+router.route("/signin/admin").post(validateSignIpRequest, isRequestValidated, adminSignin);
 
 module.exports = router;
