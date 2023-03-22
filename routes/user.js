526fff5c6320d67132ad8080e6b259e7ce7e7591
shubhcode12 <shubhcode.dev@gmail.com>
2023-03-22 18:50:40 +0530

    Add : reviews list api

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
+const Review = require("../models/review.model");
 
+// Users listing api
 router.route("/users").get(async function (req , res){
     const result = await User.find();
     res.json(result)
 });
+
+// Riviews list api
+router.route("/reviews").get(async function(req , res){
+    const result = await Review.find();
+    res.json(result); 
+})
 
 module.exports = router;
