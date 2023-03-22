abafa2d4967c37adf2eff6f3b287348b054f028e
shubhcode12 <shubhcode.dev@gmail.com>
2023-03-22 19:12:12 +0530

    Add : Review add Api

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
 const Review = require("../models/review.model");
+const { default: mongoose } = require("mongoose");
 
 // Users listing api
 router.route("/users").get(async function (req , res){
@@ -13,6 +14,27 @@ router.route("/users").get(async function (req , res){
 router.route("/reviews").get(async function(req , res){
     const result = await Review.find();
     res.json(result); 
-})
+});
+
+// Add Review by name, role & review Api
+router.route("/reviews/add").post(async function (req, res) {
+    const review = new Review({
+        _id: new mongoose.Types.ObjectId,
+        name: req.body.name,
+        role: req.body.role,
+        review: req.body.review,
+    });
+    review.save().then((result) => {
+        res
+            .status(200)
+            .json({ newReview: result });
+
+    }).catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    });
+});
+
 
 module.exports = router;
